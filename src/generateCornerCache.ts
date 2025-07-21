f498fcf196556dfb94e8069329aaa2c074c3c6ed
Evan Zhou <evanzhoudev@gmail.com>
2025-07-20 20:42:27 -0700

    ✨ Add script to cache all corner permutations as even or odd

---
+++ src/generateCornerCache.ts
@@ -0,0 +1,27 @@
+import { Permutation } from "js-combinatorics";
+import { getFactoradic, getParity } from "./util";
+
+export function generateCornerCache() {
+	const evenPerms: Array<number> = []; // array of the factoradic number representation of EVEN permutations in order
+	const oddPerms: Array<number> = []; // array of the factoradic number representation of ODD permutations in order
+	const getEvenPermIndex = {}; // map from the factoradic number representation to the index of that factoradic number in the evenPerms array
+	const getOddPermIndex = {}; // map from the factoradic number representation to the index of that factoradic number in the oddPerms array
+
+	let it = new Permutation(new Array(8).fill(null).map((x, i) => i));
+	for (const elem of it) {
+		const parity = getParity(elem);
+		const factoradic = getFactoradic(elem);
+		if (parity == 0) {
+			evenPerms.push(factoradic);
+			getEvenPermIndex[factoradic] = evenPerms.length - 1;
+		} else if (parity == 1) {
+			oddPerms.push(factoradic);
+			getOddPermIndex[factoradic] = oddPerms.length - 1;
+		}
+	}
+
+	return { evenPerms, oddPerms, getEvenPermIndex, getOddPermIndex };
+}
+
+import fs from "fs";
+fs.writeFileSync("./cornerCache.json", JSON.stringify(generateCornerCache()));
