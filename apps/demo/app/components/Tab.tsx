e6eef7f152727290b019bf62cfe952ab67defd0b
Evan Zhou <evanzhoudev@gmail.com>
2025-07-21 15:11:01 -0700

    ✨ Add component for the CubeCode website tabs

---
+++ apps/demo/app/components/Tab.tsx
@@ -0,0 +1,30 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Tab({
+	title,
+	url,
+	color,
+}: {
+	title: string;
+	url: string;
+	color: string;
+}) {
+	const pathname = usePathname();
+	const isActive = pathname === url;
+
+	return (
+		<Link
+			href={url}
+			className={`flex flex-col items-center justify-center flex-1`}
+		>
+			<p
+				className={`geo text-3xl sm:text-5xl ${isActive ? `opacity-100` : "opacity-20 text-black"}`}
+				style={isActive ? { color } : {}}
+			>
+				{title}
+			</p>
+		</Link>
+	);
+}
