19ff401c6dca2855c3be8381a47fc575684d519f
Evan Zhou <evanzhoudev@gmail.com>
2025-07-20 20:49:54 -0700

    ✨ Have a single index.ts which has exports to encode, decode, and generate the corner cache

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { encodeCube } from "./src/encodeCube";
+import { decodeCube } from "./src/decodeCube";
+import { generateCornerCache } from "./src/generateCornerCache";
+
+export { encodeCube, decodeCube, generateCornerCache };
